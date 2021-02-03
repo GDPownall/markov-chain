97fddf0f9762ab7619c9fb5832f4ee1cd332a84d
gpownall <gavin.pownall@cern.ch>
2021-02-03 19:29:30 +0000

    Fix timer

--- time_monopoly.py
+++ time_monopoly.py
@@ -4,18 +4,18 @@ from Monopoly import *
 import time
 import psutil
 
-start_CPU = psutil.virtual_memory().used
-print('CPU at start' , start_CPU/10**6)
+start_RAM = psutil.virtual_memory().used
+print('RAM at start' , start_RAM/10**6)
 
 start_time = time.time()
 x = Monopoly_MarkovChain()
 x.markov.CalcStateAtTime(40)
 print('Time to calculate state at 40 iterations with Markov Chain: ',time.time()-start_time)
-used_CPU = (psutil.virtual_memory().used - start_CPU)/10**6
-print('CPU usage:', used_CPU)
+used_RAM = (psutil.virtual_memory().used - start_RAM)
+print('RAM usage:', used_RAM*10**-6)
 
 start_time = time.time()
 monte = Monopoly_MonteCarlo(100000)
 monte.CalcStateAtTime(40)
 print('Time to calculate state at 40 iterations with Monte Carlo: ',time.time()-start_time)
-print('CPU usage:',(psutil.virtual_memory().used - used_CPU)/10**6)
+print('RAM usage:',(psutil.virtual_memory().used - used_RAM - start_RAM)*10**-6)
