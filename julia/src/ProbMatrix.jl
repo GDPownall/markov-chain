659c37937ca7e3253166d0f815b3ca37b31d8d66
gpownall <gavin.pownall@cern.ch>
2021-06-13 15:18:37 +0100

    Move files around, create julia and python directories, initialise file for julia ProbMatrix type

---
+++ julia/src/ProbMatrix.jl
@@ -0,0 +1,29 @@
+# This file contains the struct for the probability matrix
+
+struct ProbMatrix
+    data::AbstractArray
+
+    function ProbMatrix(data) 
+	if ndims(data) != 2
+		error("Probability matrix needs to be two-dimensional")
+	end
+	if size(data,1) != size(data,2)
+		error("Probability matrix needs to be square.")
+	end
+	for col_num in 1:size(data,1)
+		if sum(data[:,col_num]) != 1
+			error("Each column of a probability matrix must sum to one.")
+		end
+	end
+        new(data)
+    end
+end
+"""
+function ProbMatrix(A::AbstractMatrix)
+    LinearAlgebra.checksquare(A)
+    return symmetric_type(typeof(A))(A, char_uplo(uplo))
+end
+"""
+Mat = [0.8 0.05 ; 0.2 0.95]
+
+A = ProbMatrix(Mat)
