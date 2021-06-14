2e3fd1ce83d2fcf19a67054c2fc39dbdd682d5b7
gpownall <gavin.pownall@cern.ch>
2021-06-14 20:24:56 +0100

    Bug fixes

--- julia/src/ProbMatrix.jl
+++ julia/src/ProbMatrix.jl
@@ -1,21 +1,31 @@
 # This file contains the struct for the probability matrix
 
-struct ProbMatrix
-    data::AbstractArray
+using LinearAlgebra
 
-    function ProbMatrix(data) 
-	if ndims(data) != 2
-		error("Probability matrix needs to be two-dimensional")
-	end
-	if size(data,1) != size(data,2)
-		error("Probability matrix needs to be square.")
-	end
-	for col_num in 1:size(data,1)
-		if sum(data[:,col_num]) != 1
-			error("Each column of a probability matrix must sum to one.")
-		end
-	end
-        new(data)
-    end
+struct ProbMatrix{T<:Real} <: AbstractArray{T,2}
+        data
+
+        function ProbMatrix{T}(data::AbstractArray{T,2}) where T<:Real
+                if ndims(data) != 2
+                        error("Probability matrix needs to be two-dimensional")
+                end
+                if size(data,1) != size(data,2)
+                        error("Probability matrix needs to be square.")
+                end
+                for col_num in 1:size(data,1)
+                        if sum(data[:,col_num]) != 1
+                                error("Each column of a probability matrix must sum to one.")
+                        end
+                end
+                new{T}(data)
+        end
 end
 
+# Outer constructors
+
+ProbMatrix(data::Array{T,2}) where {T} = ProbMatrix{T}(data)
+
+# Define functions
+
+Base.size(m::ProbMatrix{T}) where T = size(m.data)
+LinearAlgebra.factorize(m::ProbMatrix{T}) where T = factorize(m.data)
