6848040d0c035b7e9a3a75301cced52165b04ec1
Gavin Pownall <gpownall95@gmail.com>
2023-06-14 14:28:06 +0100

    Make streamlit app multipage

---
+++ src/markov_chain/apps/streamlit/main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.title("Front page")
+
+st.text(
+    "Welcome to my Eigenvector Centrality app! \n"
+    "In the pages in the sidebar, I give examples of how Markov Chain methods can be used\n"
+    "Please take a browse!"
+)
+
+st.text("Author: Gavin Pownall \n" "Email: gavindpownall@gmail.com")
